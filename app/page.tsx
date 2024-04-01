a1ff7d6b79cbde90afdb0f9e55b8082675344ae2
luke7231 <tmddnjs7231@gmail.com>
2024-04-01 20:07:34 +0900

    feat: 파일 선택 후 최적화 기능 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,101 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import React, { useState } from "react";
+import { insertPages } from "./lib/action";
+import { mergeArrays } from "./lib/utils";
+import { compressFile, cropImage, uploadToS3 } from "./lib/file-utils";
+
+const ImageUpload = () => {
+  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+  const [previewUrls, setPreviewUrls] = useState<any>([]);
+  const [result, setResult] = useState<any>([]);
+  const [loading, setLoading] = useState(false);
+
+  const filesSelectedHandler = (event: any) => {
+    const files = Array.from(event.target.files);
+    setSelectedFiles(files as File[]);
+
+    const previews = files.map((file) => URL.createObjectURL(file as Blob));
+    setPreviewUrls(previews);
+  };
+
+  const handleOpti = async () => {
+    if (selectedFiles.length === 0) {
+      alert("이미지를 선택하세요.");
+      return;
+    }
+
+    setLoading(true);
+
+    // 🧹 이미지 최적화
+    try {
+      const compressedFilesPromises = selectedFiles.map((file: File) => {
+        return compressFile(file, 0.8);
+      });
+
+      // 압축된 이미지로 선택된 파일 업데이트
+      Promise.all(compressedFilesPromises)
+        .then((compressedFiles: File[]) => {
+          // 새로 압축된 파일로 선택된 파일 업데이트
+          setSelectedFiles(compressedFiles);
+          console.log("최적화 완료 🚀");
+        })
+        .catch((error: any) => {
+          console.error("압축된 이미지를 업데이트하는 도중 오류 발생:", error);
+        });
+    } catch (error) {
+      console.error(error);
+    }
+
+    setLoading(false);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="p-24">
+      <input
+        type="file"
+        accept="image/*"
+        onChange={filesSelectedHandler}
+        multiple
+      />
+      <div className="flex gap-4 ">
+        {previewUrls.map((previewUrl: string, index: number | string) => (
+          <img
+            key={index}
+            src={previewUrl}
+            alt={`Preview ${index}`}
+            className="border border-solid border-black"
+            style={{ maxWidth: "100%", maxHeight: "200px" }}
+          />
+        ))}
+      </div>
+      <div className="flex gap-4 mt-12">
+        {result.map(
+          (
+            resultImageUrl: { img: string; key: string; thumbnail: string },
+            index: number | string
+          ) => (
+            <div key={index}>
+              <img
+                key={index}
+                src={resultImageUrl.thumbnail}
+                alt={`Result ${index}`}
+                style={{ maxWidth: "100%", maxHeight: "200px" }}
+              />
+              {/* <h3>{resultImageUrl}</h3> */}
+            </div>
+          )
+        )}
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <button
+        className="mt-4 mr-4 border border-black p-2 rounded-lg"
+        onClick={handleOpti}
+      >
+        최적화 🚀
+      </button>
+    </div>
   );
-}
+};
+
+export default ImageUpload;
