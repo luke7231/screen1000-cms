f428a44257e742743c2f3ebb07862f6645f7da56
luke7231 <tmddnjs7231@gmail.com>
2024-04-01 19:31:37 +0900

    chore: delete 주석

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,8 +63,6 @@ export default function RootLayout({
         </header>
         {children}
       </body>
-      {/* Pretendard test */}
-      {/* <body className={pretendard.className}>{children}</body> */}
     </html>
   );
 }
